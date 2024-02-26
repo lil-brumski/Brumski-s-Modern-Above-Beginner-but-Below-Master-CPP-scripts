a19082dfbf493b77e4f3e3a45082691ba6cf169e
David Tamaratare Oghenebrume <151965409+lil-brumski@users.noreply.github.com>
2024-02-26 06:23:16 +0100

    Add files via upload

---
+++ using classes.cxx
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <limits>
+#include <string>
+
+template <typename T>
+  T getInput(const std::string& prompt){
+      T value;
+      while(!(std::cout<<prompt && std::cin>>value)){
+          std::cin.clear();
+          std::cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
+          std::cerr<<"Try again."<<std::endl;
+      }
+      return value;
+  }
+
+template <>
+std::string getInput<std::string>(const std::string& prompt){
+    std::string value;
+    std::cout<<prompt;
+    std::getline(std::cin,value);
+    return value;
+}
+     
+  class Boy{
+  public:
+      std::string name;
+      int age;
+      bool e18_or_above;
+      
+      void about(){
+              if(e18_or_above){
+                  std::cout<<"Name: "<<name<<".\nAge: "<<age<<".\nStudent: Yes"<<std::endl;
+              }
+              else{
+                  std::cout<<"Name: "<<name<<".\nAge: "<<age<<".\nStudent: Yes"<<std::endl;
+              }
+      }
+      
+  };
+  
+  int main(){
+      Boy boy2;
+      boy2.name = getInput<std::string>("You are: ");
+      boy2.age = getInput<int>("Your age: ");
+      boy2.e18_or_above = getInput<bool>("Are you a student? 1 for yes, 0 for no: ");
+      
+      boy2.about();
+  }
