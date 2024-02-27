193c65cab2711056d84cf26d7fce7b74562f2a5d
David Tamaratare Oghenebrume <151965409+lil-brumski@users.noreply.github.com>
2024-02-27 07:16:37 +0100

    Update your name.cpp

--- your name.cpp
+++ your name.cpp
@@ -2,6 +2,7 @@
 #include <limits>
 #include <string>
 
+//This template allows users to reinput a variable value after entering the wrong data type.
 template <typename T>
   T getInput(const std::string& prompt){
       T value;
@@ -13,7 +14,7 @@ template <typename T>
           return value;
   }
   
-  
+ //This template allows the user to enter strings on the same line but included with spaces. The whole line is one string variable value.
   template <>
     std::string getInput<std::string>(const std::string& prompt){
         std::string value;
@@ -23,7 +24,7 @@ template <typename T>
         return value;
     }
     
-
+ //Class name called "Student". A class is a blueprint for creating objects.
 class Student{
     public:
     std::string name;
@@ -32,9 +33,11 @@ class Student{
     std::string likes;
             void about(){
               if(gender){
+                   //If the gender is 1(male), then this code will run.
                    std::cout<<"\nYour name is "<<name<<".\nYou are "<<age<<" years old.\nYou are a male.\nYour interest(s): "<<likes<<std::endl;
                  }
              else{
+                      //If the gender is 0(female), then this one will run.
                       std::cout<<"\nYour name is "<<name<<".\nYou are "<<age<<" years old.\nYou are a female.\nYour interest(s): "<<std::endl;
                   }
            }
@@ -42,10 +45,15 @@ class Student{
 
 
 int main(){
+    
+    //The class "Student" with its object "details".
     Student details;
+    //The variables of "Students" belong to "details" now.
     details.name = getInput<std::string>("Enter your name: ");
     details.age = getInput<int>("Enter your age: ");
     details.gender = getInput<bool>("Enter your gender(1 for male and 0 for female): ");
     details.likes = getInput<std::string>("Enter your interest(s): ");
+    
+    //This calls the "about();" function in the "Student" class.
     details.about();
 }
