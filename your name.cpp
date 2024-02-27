9264ff00b7462bee42ea6dfe08aec959a017167d
David Tamaratare Oghenebrume <151965409+lil-brumski@users.noreply.github.com>
2024-02-27 07:01:53 +0100

    Add files via upload

---
+++ your name.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <limits>
+
+template <typename T>
+  T getInput(const std::string& prompt){
+      T value;
+          while(!(std::cout<<prompt && std::cin>>value)){
+              std::cin.clear();
+              std::cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
+              std::cerr<<"Invalid input!"<<std::endl;          
+          }
+          return value;
+  }
+  
+  
+  template <>
+    std::string getInput<std::string>(const std::string& prompt){
+        std::string value;
+        std::cout<<prompt;
+        std::cin.ignore();
+        std::getline(std::cin,value);
+        return value;
+    }
+    
+
+class Student{
+    public:
+    std::string name;
+    int age;
+    bool gender;
+    std::string likes;
+            void about(){
+              if(gender){
+                   std::cout<<"\nYour name is "<<name<<".\nYou are "<<age<<" years old.\nYou are a male.\nYou like: "<<likes<<std::endl;
+                 }
+             else{
+                      std::cout<<"\nYour name is "<<name<<".\nYou are "<<age<<" years old.\nYou are a female."<<std::endl;
+                  }
+           }
+};
+
+
+int main(){
+    Student details;
+    details.name = getInput<std::string>("Enter your name: ");
+    details.age = getInput<int>("Enter your age: ");
+    details.gender = getInput<bool>("Enter your gender(1 for male and 0 for female): ");
+    details.likes = getInput<std::string>("Enter your interest(s): ");
+    details.about();
+}
